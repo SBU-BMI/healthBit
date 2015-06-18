1faca754c7fe856c7551bd263a3d657c4efbca77
Jonas Almeida <jalmeida@mathbiol.org>
2015-06-18 11:13:48 -0400

    ref to dev tools for human api

--- hAPI/hAPI.js
+++ hAPI/hAPI.js
@@ -1,1 +1,22 @@
 console.log('hAPI loaded')
+
+hAPI={
+    dobra:function(x){
+        return 2*x
+    },
+    connectBtn:function(){ // create connect button
+        var img = document.createElement('img')
+        img.id='connect-health-data-btn' 
+        img.src='https://connect.humanapi.co/assets/button/blue.png'
+        return img
+    },
+    ini:function(){
+        console.log('initiatilizng Human API ...')
+        
+        hAPIdiv.appendChild(this.connectBtn())
+
+
+    }
+}
+
+hAPI.ini()
